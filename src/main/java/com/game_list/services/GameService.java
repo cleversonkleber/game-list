6676bdde89d35db60c4aed10df0d7ac7506d7ef6
cleverson Cordeiro <cleversonkleber@gmail.com>
2024-11-19 22:59:38 -0300

    Ajustes postgresql

--- src/main/java/com/game_list/services/GameService.java
+++ src/main/java/com/game_list/services/GameService.java
@@ -3,6 +3,7 @@ package com.game_list.services;
 import com.game_list.dtos.GameDTO;
 import com.game_list.dtos.GameMinDTO;
 import com.game_list.entities.Game;
+import com.game_list.projections.GameMinProjection;
 import com.game_list.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,4 +29,11 @@ public class GameService {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(GameMinDTO::new).toList();
+    }
+
 }
